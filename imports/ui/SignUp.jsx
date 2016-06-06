be8a413066b3937bc90418d9eaaa6668debbd913
BeijiYang <langyuyang@outlook.com>
2016-06-06 18:09:36 +0800

    meteor-react

---
+++ imports/ui/SignUp.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
+class Hello extends Component {
+  render() {
+    return (
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <RaisedButton label='material-ui' primary={true} />
+      </MuiThemeProvider>
+      );
+  }
+}
+
+export default Hello;
